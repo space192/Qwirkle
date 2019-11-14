86fbc17da3514c022ef7baf194b8b3b20670f21a
space192 <arthur.g.fournier@gmail.com>
2019-11-14 20:28:12 +0100

    Ajout de fonctions
    Ajout des fonctions sauvegardes dans fichier + lecture des regles pour utilisation depuis le menu.
    Premiere ecriture des regles dans fichier .txt

---
+++ constantes.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTES_H_INCLUDED
+#define CONSTANTES_H_INCLUDED
+
+typedef struct JOUEUR
+{
+    char nom[20];
+    int score;
+    char tuiles[6][3];
+}T_JOUEUR;
+
+#endif // CONSTANTES_H_INCLUDED
---
+++ fichier.c
@@ -0,0 +1,40 @@
+#include "constantes.h"
+
+void affichageRegle()
+{
+    FILE* regles = NULL;
+    regles = fopen("regles.txt", "r");
+    if(regles == NULL)
+    {
+        printf("erreur dans l'affichage des regles peut etre que le fichier regle est manquant.\n");
+    }
+    else
+    {
+        char chaineRegle[1000];
+        while(fgets(chaineRegle, 1000, regles) != NULL)
+        printf("%s", chaineRegle);
+        fclose(regles);
+    }
+
+}
+
+void sauvegardeScore(T_JOUEUR joueur[4])
+{
+    FILE* score = NULL;
+    int i;
+    score = fopen("scores.txt", "a");
+    if(regles == NULL)
+    {
+        printf("erreur lors de la sauvegarde des scores peut etre que le fichier score est manquant.\n");
+    }
+    else
+    {
+        for(i = 0 ; i < 4 ; i++)
+        {
+            fputs(joueur[i].nom, score);
+            fputs(joueur[i].score, score);
+            fputs(joueur.tuiles, score);
+        }
+        fclose(score);
+    }
+}
