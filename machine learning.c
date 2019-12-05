a7c3852b5c49f3c9e40d07fef0085b03fa05d67b
space192 <arthur.g.fournier@gmail.com>
2019-12-05 13:38:47 +0100

    ajout machine learning

---
+++ machine learning.c
@@ -0,0 +1,11 @@
+#include "prototypes.h"
+#include "prototypes.h"
+
+void IA(T_TUILE plateau[][27], T_TUILE main[][6])
+{
+    printf("premiere essaie IA sur le qwirkle");
+    srand(time(0));
+    int i;
+
+    genann *ann = genann_init()
+}
