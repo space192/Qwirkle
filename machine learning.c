6438086a2349d3dd15a1aef89f3d04fe94c196f3
space192 <arthur.g.fournier@gmail.com>
2019-12-06 00:38:16 +0100

    jii

--- machine learning.c
+++ machine learning.c
@@ -1,37 +1,22 @@
 #include "prototypes.h"
 
-int evaluer(T_TUILE plateau[13][27], T_TUILE main[][6], int coordX[6], int coordY[6])
+int evaluer(T_TUILE plateau[13][27], T_TUILE main[][6], int coordX, int coordY)
 {
-    int i, j,k, x, y;
+    int i, j,k=8, x, y;
+    int scoreIteration[6];
+    int possibilite[6];
     int lockC=1, lockF=1;//prototype de test : test(int x,int y,T_TUILE main[][6],T_TUILE plateau[][27],int joueurActif, int tuileJoueur, int *lockC, int *lockF, int *pscore)
     int scoreCoup;
     for(i=0; i < 6 ; i++)//parcour des tuiles de la main du joueur
     {
-        switch(i)
-        {
-        case 0:
-            x=1;
-            y=0;
-            break;
-        case 1:
-            x=-1;
-            y=0;
-            break;
-        case 2:
-            x=0;
-            y=1;
-            break;
-        case 3:
-            x=0;
-            y=-1;
-            break;
-        }
-
-        for(j=0; j < 6 ; j++)//parcours des position autour des tuiles placé par le joueur précedent
-        {
-            test(coordX[j]+x, coordY[k]+y, main, plateau, 0, i, &lockC, &lockF, &scoreCoup);
-        }
-
+            possibilite[i]=test(coordX+1, coordY, main, plateau, 1, i, &lockC, &lockF, &scoreIteration[i]);
+    }
+    for(i=0; i < 6; i++)
+    {
+        gotoligcol(k, 60);
+        k++;
+        printf("possibilite iteration %d et score iteration%d", possibilite[i], scoreIteration[i]);
+        gotoligcol(x, y);
     }
 
 }
