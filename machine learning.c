b31107c2630715b4c1af5e6f3b85129d712c6ba4
space192 <arthur.g.fournier@gmail.com>
2019-12-05 23:27:10 +0100

    oiz

--- machine learning.c
+++ machine learning.c
@@ -2,7 +2,9 @@
 
 int evaluer(T_TUILE plateau[13][27], T_TUILE main[][6], int coordX[6], int coordY[6])
 {
-    int i, j,score, x, y;
+    int i, j,k, x, y;
+    int lockC=1, lockF=1;
+    int scoreCoup;
     for(i=0; i < 6 ; i++) //prototype de test : test(int x,int y,T_TUILE main[][6],T_TUILE plateau[][27],int joueurActif, int tuileJoueur, int *lockC, int *lockF, int *pscore);
     {
         switch(i)
@@ -27,7 +29,10 @@ int evaluer(T_TUILE plateau[13][27], T_TUILE main[][6], int coordX[6], int coord
 
         for(j=0; j < 6 ; j++)
         {
-
+            for(k=0; k < 6 ;k++)
+            {
+                test(coordX[j]+x, coordY[k]+y, main, plateau, 0, i, &lockC, &lockF, &scoreCoup);
+            }
         }
     }
 
