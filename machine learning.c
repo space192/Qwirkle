d4079577c529178b5494d6e057bf867aefb4a0f6
space192 <arthur.g.fournier@gmail.com>
2019-12-06 17:36:41 +0100

    UNE IA EST NE

--- machine learning.c
+++ machine learning.c
@@ -1,21 +1,95 @@
 #include "prototypes.h"
 
-int evaluer(T_TUILE plateau[][27], T_TUILE main[][6], int coordX, int coordY)
+int calculerCoup(T_TUILE plateau[][27], T_TUILE tuile[][6], int coordX, int coordY, int lockC, int lockF)
 {
-    int i,k=8;
-    int scoreIteration[6];
-    int possibilite[6];
-    int lockC=1, lockF=1;//prototype de test : test(int x,int y,T_TUILE main[][6],T_TUILE plateau[][27],int joueurActif, int tuileJoueur, int *lockC, int *lockF, int *pscore)
-    for(i=0; i < 6 ; i++)//parcour des tuiles de la main du joueur
-    {
-        possibilite[i]=test(coordX, coordY, main, plateau, 0, i, &lockC, &lockF, &scoreIteration[i]);
-    }
+    int i,j,k=0,m=8, scoreCoup, x,y, scoreCoupParticulier[24];
+    int compteurCouleur[6], compteurForme[6];
+    char carre = 0xFE, losange = 0x04, trefle = 0x05, pique =  0x06, triangle = 0x1E, croix = 0x9E;
     for(i=0; i < 6; i++)
     {
-        gotoligcol(k, 60);
-        k++;
-        printf("possibilite iteration %d et score iteration%d", possibilite[i], scoreIteration[i]);
-        gotoligcol(coordX, coordY);
+        switch(tuile[1][i].couleur)
+        {
+        case 1:
+            compteurCouleur[0]++;
+            break;
+        case 2:
+            compteurCouleur[1]++;
+            break;
+        case 3:
+            compteurCouleur[2]++;
+            break;
+        case 4:
+            compteurCouleur[3]++;
+            break;
+        case 5:
+            compteurCouleur[4]++;
+        case 6:
+            compteurCouleur[5]++;
+        }
+        if(tuile[1][i].forme == carre)
+        {
+            compteurForme[0]++;
+        }
+        else if(tuile[1][i].forme == losange)
+        {
+            compteurForme[1]++;
+        }
+        else if(tuile[1][i].forme == trefle)
+        {
+            compteurForme[2]++;
+        }
+        else if(tuile[1][i].forme == pique)
+        {
+            compteurForme[3]++;
+        }
+        else if(tuile[1][i].forme == triangle)
+        {
+            compteurForme[4]++;
+        }
+        else if(tuile[1][i].forme == croix)
+        {
+            compteurForme[5]++;
+        }
     }
-
+    for(i=0; i < 6 ; i++)
+    {
+        x=0;
+        y=0;
+        lockC=1;
+        lockF=1;
+        for(j=0; j < 4 ; j++)
+        {
+            switch(j)
+            {
+            case 0:
+                x= 1;
+                y= 0;
+                break;
+            case 1:
+                x= -1;
+                y= 0;
+                break;
+            case 2:
+                x= 0;
+                y= 1;
+                break;
+            case 3:
+                x= 0;
+                y= -1;
+            }
+            test(coordX+x,coordY+y, tuile, plateau, 1, i, &lockC, &lockF, &scoreCoup);
+            if(lockC == 0 || lockF == 0)
+            {
+                scoreCoup++;
+            }
+            scoreCoupParticulier[k]=scoreCoup;
+            k++;
+        }
+    }
+    for(i=0; i < 24;i++)
+    {
+    gotoligcol(m, 100);
+    printf("%d et %d", i+1, scoreCoupParticulier[i]);
+    m++;
+    }
 }
